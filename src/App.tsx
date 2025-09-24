d0200b7c8978af0bb7ad07046456c6902736f2fb
Mikel Arrastia <mikelarras@gmail.com>
2025-09-25 00:01:24 +0200

    chore: extrae la carta a un componente propio

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import "./App.css";
 import logo from "./assets/logo.svg";
+import { PlantService } from "./core/services/plantService";
+import { useState, useEffect } from "react";
+import { type Plant } from "./core/domain/Plant";
+import { ApiPlantRepository } from "./core/infrastructure/ApiPlantRepository";
+import { Card } from "./components/Card";
 
 function App() {
+  const [plants, setPlants] = useState<Plant[]>([]);
+
+  useEffect(() => {
+    const plantService = new PlantService(ApiPlantRepository);
+    plantService.getPlants().then((resp) => {
+      setPlants(resp);
+    });
+  }, []);
+
   return (
     <>
       <header>
         <img src={logo} alt="logo tienda" />
       </header>
+      <main>
+        <div className="searchBar">
+          <input type="text" placeholder="Busca en nuestra tienda"></input>
+        </div>
+        <div className="cardContainer">
+          <ul>
+            {plants.map((plant) => {
+              return (
+                <li>
+                  <Card plant={plant}></Card>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
     </>
   );
 }
