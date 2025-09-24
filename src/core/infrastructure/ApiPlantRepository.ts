85d19143abcc753233b50e7dabe00d80afb62cc5
Mikel Arrastia <mikelarras@gmail.com>
2025-09-24 20:34:01 +0200

    chore: transforma salida de la api a mi formato

--- src/core/infrastructure/ApiPlantRepository.ts
+++ src/core/infrastructure/ApiPlantRepository.ts
@@ -1,19 +1,47 @@
 import { type PlantRepository } from "../domain/PlantRepository";
+import { type Plant } from "../domain/Plant";
 
 const API_BASE_URL = "https://dulces-petalos.jakala.es";
+interface ApiPlant {
+  binomialName: string;
+  fertilizerType: string;
+  id: string;
+  imgUrl: string;
+  name: string;
+  price: number;
+  wateringsPerWeek: number;
+}
 
 export const ApiPlantRepository: PlantRepository = {
   listPlants: async () => {
     const path = `${API_BASE_URL}/api/v1/product`;
     const apiPlantsResponse = await fetch(path);
     const apiPlants = await apiPlantsResponse.json();
-    return apiPlants;
+    const plants = apiPlants.map((apiPlant: ApiPlant) =>
+      transformPlant(apiPlant)
+    );
+    return plants;
   },
 
   listPlantById: async (plantId: string) => {
     const path = `${API_BASE_URL}/api/v1/product/${plantId}`;
     const apiPlantResponse = await fetch(path);
     const apiPlant = await apiPlantResponse.json();
-    return apiPlant;
+    const plant = transformPlant(apiPlant);
+    return plant;
   },
+};
+
+const transformPlant = (apiPlant: ApiPlant): Plant => {
+  return {
+    id: apiPlant.id,
+    name: apiPlant.name,
+    scientificName: apiPlant.binomialName,
+    image: apiPlant.imgUrl,
+    price: apiPlant.price,
+    water: `Regar ${apiPlant.wateringsPerWeek} ${
+      apiPlant.wateringsPerWeek === 1 ? "vez" : "veces"
+    } por semana`,
+    fertilizer: apiPlant.fertilizerType,
+  };
 };
