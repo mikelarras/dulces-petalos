25da09138db9ea02c1c183bfb252f13fc3a65179
Mikel Arrastia <mikelarras@gmail.com>
2025-09-24 18:31:15 +0200

    chore: importa react-testing-library y configura archivos necesarios

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -10,4 +9,9 @@ export default defineConfig({
       },
     }),
   ],
-});
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/tests/setup.ts",
+  },
+} as UserConfig);
