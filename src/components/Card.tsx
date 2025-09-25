252ae03c98e986c7a58f9f03a08016be5efaaeaa
Mikel Arrastia <mikelarras@gmail.com>
2025-09-25 22:11:53 +0200

    chore: actualiza componente Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,24 @@
 import { type Plant } from "../core/domain/Plant";
+import arrow from "../assets/arrow.svg";
 
 export const Card = ({ plant }: { plant: Plant }) => {
   return (
     <>
-      <h2 className="nunito-700">{plant.name}</h2>
-      <p className="dm-sans-400">{plant.scientificName}</p>
-      <img src={plant.image} alt="" />
+      <div className="card-text">
+        <h2 className="nunito-700">{plant.name}</h2>
+        <p className="dm-sans-400">{plant.scientificName}</p>
+      </div>
+      <div>
+        <img src={plant.image} alt="" />
+        <div className="floating-items">
+          <div className="price">
+            <p className="nunito-500">€ {plant.price}</p>
+          </div>
+          <div className="arrow">
+            <img src={arrow} alt="" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
