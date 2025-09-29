b9369e264492398eb91b416a44fa2f46d1de929c
Mikel Arrastia <mikelarras@gmail.com>
2025-09-29 02:10:28 +0200

    fix: restaura configuracion de vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = "dulces-petalos-mikel-arrastia";
-
 export default defineConfig({
-  base: `/${repoName}/`,
   plugins: [
     react({
       babel: {
